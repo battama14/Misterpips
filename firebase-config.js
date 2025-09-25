496a767785b4bb34fe2962be5262137ef4902ce6
battama14 <169171805+battama14@users.noreply.github.com>
2025-09-25 12:20:40 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Remplacez ces valeurs par celles de votre projet Firebase
+export const firebaseConfig = {
+    apiKey: "VOTRE_API_KEY",
+    authDomain: "VOTRE_PROJECT_ID.firebaseapp.com",
+    projectId: "VOTRE_PROJECT_ID",
+    storageBucket: "VOTRE_PROJECT_ID.appspot.com",
+    messagingSenderId: "VOTRE_SENDER_ID",
+    appId: "VOTRE_APP_ID"
+};
+
+// Exemple de ce que vous devriez avoir :
+// apiKey: "AIzaSyC1234567890abcdefghijklmnopqrstuvw",
+// authDomain: "misterpips-auth.firebaseapp.com",
+// projectId: "misterpips-auth",
+// storageBucket: "misterpips-auth.appspot.com",
+// messagingSenderId: "123456789012",
+// appId: "1:123456789012:web:abcdef123456789"
